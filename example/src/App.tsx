46e6d4a428653e158c48f6699784cfcadb77309a
David Govea <govea.d@gmail.com>
2020-02-06 15:54:26 -0800

    fix: tslint refactor for no-jsx-lambdas

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,26 +3,29 @@ import { View, Alert, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { checkPackage } from 'react-native-android-appcheck';
 
 const App: React.FunctionComponent = () => {
-  const testAppChecker = useCallback((packageName) => {
-    checkPackage(packageName)
-      .then((isInstalled) => {
-        Alert.alert(`${packageName} installed?`, `${isInstalled}`);
-      })
-      .catch((e: any) => {
-        Alert.alert('Error', e?.message || e || 'Unknown error');
-      });
-  }, []);
+  const testAppChecker = useCallback(
+    (packageName) => () => {
+      checkPackage(packageName)
+        .then((isInstalled) => {
+          Alert.alert(`${packageName} installed?`, `${isInstalled}`);
+        })
+        .catch((e: Error | string | undefined) => {
+          Alert.alert('Error', e?.message || e || 'Unknown error');
+        });
+    },
+    []
+  );
 
   return (
     <View style={styles.container}>
       <Text style={{ textAlign: 'center' }}>react-native-android-appcheck example</Text>
       <TouchableOpacity>
-        <Text style={styles.button} onPress={() => testAppChecker('com.apple.android.music')}>
+        <Text style={styles.button} onPress={testAppChecker('com.apple.android.music')}>
           Check for Apple Music
         </Text>
       </TouchableOpacity>
       <TouchableOpacity>
-        <Text style={styles.button} onPress={() => testAppChecker('com.ubercab')}>
+        <Text style={styles.button} onPress={testAppChecker('com.ubercab')}>
           Check for Uber
         </Text>
       </TouchableOpacity>
