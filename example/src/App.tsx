3f74f7c184548e25d5f092dfedeb05e38e66eda5
David Govea <govea.d@gmail.com>
2020-02-06 13:27:06 -0800

    feat: set up example app for basic tests

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,40 @@
-import React from 'react'
-import {
-  CheckPackage
-} from 'react-native-android-appcheck'
+import React, { useEffect, useCallback } from 'react';
+import { View, Alert, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { checkPackage } from 'react-native-android-appcheck';
 
-class App extends React.Component<{}> {
+const App: React.FunctionComponent = () => {
+  const testAppChecker = useCallback((packageName) => {
+    checkPackage(packageName)
+      .then((isInstalled) => {
+        Alert.alert(`${packageName} installed?`, `${isInstalled}`);
+      })
+      .catch((e: any) => {
+        Alert.alert('Error', e?.message || e || 'Unknown error');
+      });
+  }, []);
 
-  componentDidMount() {
-    CheckPackage.show('CheckPackage')
-  }
+  useEffect(() => testAppChecker('com.apple.android.music'), []);
 
-  render() {
-    return false
-  }
-}
+  return (
+    <View style={styles.container}>
+      <Text style={{ textAlign: 'center' }}>react-native-android-appcheck example</Text>
+      <TouchableOpacity>
+        <Text style={styles.button} onPress={() => testAppChecker('com.apple.android.music')}>
+          Check for Apple Music
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.button} onPress={() => testAppChecker('com.ubercab')}>
+          Check for Uber
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-export default App
+export default App;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, marginTop: 50, marginBottom: 50, justifyContent: 'space-around' },
+  button: { fontWeight: 'bold', textAlign: 'center', padding: 10 },
+});
