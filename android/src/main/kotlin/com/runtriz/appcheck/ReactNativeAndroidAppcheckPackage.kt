0a3e04179f3dbef6c12daa342f884d6ef328de25
David Govea <govea.d@gmail.com>
2020-02-06 16:32:09 -0800

    fix: kotlin lint - unused import

--- android/src/main/kotlin/com/runtriz/appcheck/ReactNativeAndroidAppcheckPackage.kt
+++ android/src/main/kotlin/com/runtriz/appcheck/ReactNativeAndroidAppcheckPackage.kt
@@ -5,8 +5,6 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 
-import com.runtriz.appcheck.CheckPackage
-
 class ReactNativeAndroidAppcheckPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
     return listOf(
